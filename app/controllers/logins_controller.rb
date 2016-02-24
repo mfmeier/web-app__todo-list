e9900c99cb3c25a7636b629293aa3eb93595fd06
Michael Meier <mmeier61@yahoo.com>
2016-02-24 17:26:26 -0600

    should have listened

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -18,15 +18,27 @@ end
 end
 
 MyApp.get "/logout" do
-  @user = nil
+  session["user_id"] = nil
 
   erb :"logins/logout"
 end
+MyApp.get "/edit_login" do
+  @user = User.find_by_id(session["user_id"])
 
-# This controller is for all the CRUD operations related to a Login.
 
-# Note that "logins" are not stored in the database. But there is still
-# a reasonable way to think about a "login" as a resource which is created
-# and deleted (i.e. 'logging out').
-# 
-# Reading and Updating a login, however, make a little less sense.
+  erb :"logins/edit_login"
+end
+MyApp.get "/finish_update_user/:place" do
+  @user = User.find_by_id(params[:place])
+  @user.name = params[:edit_name]
+  @user.email = params[:edit_email]
+  @user.password = params[:edit_password]
+  @user.save
+
+  erb :"logins/update_success"
+end
+MyApp.get "/administrator" do
+  @user = User.all
+
+  erb :"users/administer"
+end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,3 +9,31 @@ MyApp.get "/todo" do
         erb :"logins/login"
     end 
 end
+MyApp.get "/finish_update_user/todo" do
+  @user = User.find_by_id(session["user_id"]) 
+  @todo = Todo.where(user_id => @user.id)
+    if @user != nil
+
+        erb :"todos/todo"
+    else
+        erb :"logins/login"
+    end 
+end
+
+MyApp.get "/add_task" do
+    @user = User.find_by_id(session["user_id"]) 
+    @todo = Todo.where(user_id => @user.id)
+  new_task = Todo.new
+  new_task.user_id = @user.id
+  new_task.title = params[:title]
+  new_task.description = params[:description]
+  new_task.save
+
+  erb :"todos/task_follower"
+end
+
+MyApp.get "/the_list" do
+  @todo = Todo.all
+
+  erb :"todos/the_list"
+end
